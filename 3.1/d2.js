5d3f49ca9ea12c45cececdbf414ffca98987aa5e
OgNadine <125201927+OgNadine@users.noreply.github.com>
2025-02-21 16:49:55 +0300

    add 2, 3

---
+++ 3.1/d2.js
@@ -0,0 +1,11 @@
+function setFlexDirection(direction) {
+    document.getElementById('flex-direction-container').style.flexDirection = direction;
+}
+
+function setFlexWrap(wrap) {
+    document.getElementById('flex-wrap-container').style.flexWrap = wrap;
+}
+
+function setJustifyContent(align) {
+    document.getElementById('justify-content-container').style.justifyContent = align;
+}
