3313400918b39b90991dc7b502dfb609f8d5cff2
Nadezhda Ogienko <ogienko@fptrade.net>
2025-03-06 23:18:38 +0300

    add lr4 and lr4.1

---
+++ 4.1/d2.js
@@ -0,0 +1,11 @@
+function setGridTemplateColumns(columns) {
+    document.getElementById('grid-template-container').style.gridTemplateColumns = columns;
+}
+
+function setJustifyContent(align) {
+    document.getElementById('justify-content-container').style.justifyContent = align;
+}
+
+function setJustifyItems(align) {
+    document.getElementById('justify-items-container').style.justifyItems = align;
+}
